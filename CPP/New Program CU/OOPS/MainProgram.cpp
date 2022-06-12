1a3e52109cd20997e31cf8b02729ce808a27d668
Suraj Kumar <surajsinhaking77@gmail.com>
2022-06-12 09:07:44 +0530

    make some change and created Dymanic Object Creation

--- CPP/New Program CU/OOPS/MainProgram.cpp
+++ CPP/New Program CU/OOPS/MainProgram.cpp
@@ -5,14 +5,23 @@ using namespace std;
 
 int main(){
     
+    // Creating object s1 of Student Class
     Student s1;
-
     s1.age = 77;
-
     cout<< s1.getter() << endl;
-
     s1.setter(88,26);
-
     s1.display();
+
+    // Dynamic Object Creation 
+
+    Student *s2 =  new Student;  // both allow
+    Student *s3 =  new Student();
+
+    s2->setter(72, 23);   // allow 
+    s2->display();
+
+    (*s3).setter(88,61);  // allow
+    (*s3).display();
+
     return 0;
 }
