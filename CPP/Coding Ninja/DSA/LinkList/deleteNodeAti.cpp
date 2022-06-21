59aa352d8faf5658370b57fee1db3ed4fcc5bf49
Suraj Kumar <surajsinhaking77@gmail.com>
2022-06-21 06:20:58 +0530

    ""

--- CPP/Coding Ninja/DSA/LinkList/deleteNodeAti.cpp
+++ CPP/Coding Ninja/DSA/LinkList/deleteNodeAti.cpp
@@ -4,12 +4,24 @@ using namespace std;
 
 node*delectionOfnodeAtiTh(node*head, int i){
     int count=0;
+    node * temp = head;
+    if(i==0){
+        temp = head->next;
+        head = temp;
+        return head;
+    }
     while(head != NULL && count<i-1){
-        head->next->next = head;
+        head = head->next;
         count++;
     }
-    
+    if(head != NULL){
+        node * a = head->next;
+        head ->next = a->next;
+        delete a;
+    }
+    return head;
 }
+
 
 int main(){
     
